95c9674557afdba8e58431ee9a9858fac587d132
vgruv <33602765+vgruv@users.noreply.github.com>
2018-03-07 18:12:05 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -19,8 +19,6 @@
 ## - From the data set in step 4, creates a second, independent tidy data set 
 ##   with the average of each variable for each activity and each subject.
 
-## library???
-
 # to download Human Activity Recognition Using Smartphones zip file
 inputFile <- "InputDS.zip"
 if (!file.exists(fileToGet)){
